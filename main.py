37446a7da1fe600a36e6e951b5498f2f501dfa22
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-07 16:22:23 +0300

    if

---
+++ main.py
@@ -0,0 +1,38 @@
+if __name__ == '__main__':
+
+    x = int(input('x: '))
+    y = int(input('y: '))
+    z = int(input('z: '))
+
+    suma = x + y + z
+    dodatok = x * y * z
+
+
+    if input(suma):
+        print('Сума чисел складає: ')
+    if input(dodatok):
+        print('Додаток чисел складає')
+
+
+
+
+
+
+    # numbers = input('Vvedit chysla').split()
+    # a, b, c = int(input[0]), int(input[1]), int(input[2])
+    #
+    # print('Vvedit 1 for max, vvedit 2 for min, vvedit 3 for seredne: ')
+    #
+    # resultat = int(numbers())
+    #
+    # if resultat == 1:
+    #     print(max(a, b, c))
+    # elif resultat == 2:
+    #     print(min(a, b, c))
+    # elif resultat == 3:
+    #     print((a + b + c) / 3)
+    # else:
+    #     print('nevirno')
+
+
+
