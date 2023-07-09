9aafd225cd16222567640e017c2754eee98b398c
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-09 19:41:20 +0300

    add

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
 if __name__ == '__main__':
 
-    x = int(input('x: '))
-    y = int(input('y: '))
-    z = int(input('z: '))
-
-    suma = x + y + z
-    dodatok = x * y * z
-
-
-
-    if input(suma):
-        print('Сума чисел складає: ')
-    if input(dodatok):
-        print('Додаток чисел складає')
-
-
-
-
-
-
-    # numbers = input('Vvedit chysla').split()
-    # a, b, c = int(input[0]), int(input[1]), int(input[2])
+    # # Завдання 1
     #
-    # print('Vvedit 1 for max, vvedit 2 for min, vvedit 3 for seredne: ')
+    # a = int(input("Введіть перше число: "))
+    # b = int(input("Введіть друге число: "))
+    # c = int(input("Введіть третє число: "))
+    # print(' 1 - Сума чисел\n 2 - Добуток чисел')
+    # choice = int(input('Ваш вибір: '))
     #
-    # resultat = int(numbers())
-    #
-    # if resultat == 1:
-    #     print(max(a, b, c))
-    # elif resultat == 2:
-    #     print(min(a, b, c))
-    # elif resultat == 3:
-    #     print((a + b + c) / 3)
+    # if choice == 1:
+    #     print('Сума: ', a + b +c)
+    # elif choice == 2:
+    #     print('Добуток: ', a * b * c)
     # else:
-    #     print('nevirno')
+    #     print('Ви зробили невірний вибір. Спробуйте ще раз! ')
 
 
 
+    # # Завдання 2
+    # a = int(input("Введіть перше число: "))
+    # b = int(input("Введіть друге число: "))
+    # c = int(input("Введіть третє число: "))
+    #
+    #
+    # print(' 1 - Максимальне число\n 2 - Мінімальне число\n 3 - Середнє арефмитичне число')
+    #
+    # choice = int(input('Ваш вибір '))
+    #
+    # if choice == 1:
+    #     print('Максимальне число: ', max(a, b, c))
+    # elif choice == 2:
+    #     print('Мінімальне число: ', min(a, b, c))
+    # elif choice == 3:
+    #     print('Середнє арефмитичне число', (a + b + c) / 3)
+    # else:
+    #     print('Невірний вибір')
+
+
+    # # Завдання 3
+    # meters = float(input("Введіть кількість метрів: "))
+    # print(" 1. Конвертація в милі\n 2. Конвертація в дюйми\n 3. Конвертація в ярди")
+    # choice = int(input("Ваш вибір: "))
+    # if choice == 1:
+    #     print(meters * 0.000621371192)
+    # elif choice == 2:
+    #     print(meters * 39.3700787)
+    # elif choice == 3:
+    #     print(meters * 1.0936133)
+    # else:
+    #     print("Невірний вибір")
